b07eba7062a74d864ffa8e46239fa7a22b22b73b
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2023-10-09 01:12:13 -0400

    english auth translations

---
+++ lang/en/pages/authentication.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Name' => 'Name',
+    'Email' => 'Email',
+    'Password' => 'Password',
+    'Confirm Password' => 'Confirm Password',
+    'Already registered?' => 'Already registered?',
+    'Register' => 'Register',
+];
